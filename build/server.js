704381214c2bf63ce2ec8b1924303a46cad1974c
Leonardo Prieto <leonardo_santos_ext_8@carrefour.com>
2023-06-14 14:31:19 -0300

    fix

--- build/server.js
+++ build/server.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = __importDefault(require("./app"));
-var port = process.env.PORT || 3131;
+var port = process.env.PORT || 21041;
 exports.default = app_1.default.listen(port, function () {
     console.log("Servidor escutando em http://localhost:".concat(port));
 });
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app"
 
-const port = process.env.PORT || 21041
+const port = process.env.PORT || 3131
 
 export default app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
